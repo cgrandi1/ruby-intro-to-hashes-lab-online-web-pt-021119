6f30099598941eacf3419a0499b876a42258457a
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-28 06:36:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -62,7 +62,7 @@ def monopoly_with_fourth_tier
 }
 
 monopoly[:rail_roads][:names][:reading_railroad] = ["mortgage_value" => "$100"]
-monopoly[:rail_roads][:names][:pennsylvania_railroad] = ["mortgage_value" => "$200"}
-monopoly[:rail_roads][:names][:b_and_o_railroad] = {"mortgage_value" => "$400"}
-monopoly[:rail_roads][:names][:shortline] = {"mortgage_value" => "$800"}
+monopoly[:rail_roads][:names][:pennsylvania_railroad] = ["mortgage_value" => "$200"]
+monopoly[:rail_roads][:names][:b_and_o_railroad] = ["mortgage_value" => "$400"]
+monopoly[:rail_roads][:names][:shortline] = ["mortgage_value" => "$800"]
 end
